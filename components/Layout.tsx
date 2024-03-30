7775915afb21c7c426f5e03014033290a3cd22f2
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-03-30 19:25:02 +0000

    clean a bit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,9 +10,9 @@ import {
   MenuButton,
   IconButton,
   MenuList,
-  MenuItem,
-  Icon,
   MenuGroup,
+  useColorMode,
+  Icon,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -25,19 +25,25 @@ function Navigation({
   isExternal,
 }: {
   link: string;
-  children: string;
+  children: React.ReactNode;
   isExternal?: boolean;
 }) {
   const router = useRouter();
+  const { colorMode } = useColorMode();
   const isActive =
     link === "/" ? router.asPath === link : router.asPath.includes(link);
+  const activeColor = colorMode === "dark" ? "white" : "black";
+  const inactiveColor = colorMode === "dark" ? "gray.400" : "gray.500";
 
   return (
-    <Link href={link} target={isExternal ? "_blank" : "_self"}>
+    <Link href={link} passHref>
       <Text
+        as="a"
+        target={isExternal ? "_blank" : "_self"}
         fontSize="lg"
-        color={isActive ? "black" : "gray.500"}
-        _hover={{ color: "black" }}
+        color={isActive ? activeColor : inactiveColor}
+        _hover={{ color: activeColor }}
+        fontWeight={isActive ? "bold" : "normal"}
       >
         {children}
       </Text>
@@ -45,16 +51,17 @@ function Navigation({
   );
 }
 
-function Layout({ children }: PropsWithChildren) {
+function Layout({ children }: PropsWithChildren<{}>) {
   return (
     <Container
       position="relative"
+      maxW="container.xl"
+      px={0}
       mt={{ base: 16, md: 20 }}
       pb={{ base: 8, md: "10em" }}
       gap={{ md: 10 }}
     >
       <Flex
-      
         position="absolute"
         right="100%"
         mr="160px"
@@ -69,7 +76,6 @@ function Layout({ children }: PropsWithChildren) {
             <Navigation link="/projects">Projects</Navigation>
             <Navigation link="/writing">Writing</Navigation>
             <Navigation link="/books">Books</Navigation>
-
           </VStack>
           <VStack align="flex-start">
             <Text fontWeight="bold" fontSize="smaller">
@@ -114,7 +120,6 @@ function Layout({ children }: PropsWithChildren) {
                 <Navigation link="/writing">Writing</Navigation>
                 <Navigation link="/books">Books</Navigation>
                 <Navigation link="/projects">Projects</Navigation>
-
               </HStack>
               <Menu>
                 <MenuButton
@@ -135,10 +140,7 @@ function Layout({ children }: PropsWithChildren) {
                   </MenuGroup>
                   <MenuGroup title="FIND ME ON">
                     <VStack align="flex-start" px={4} spacing={3} mb={2}>
-                      <Navigation
-                        link="https://twitter.com/amitoser"
-                        isExternal
-                      >
+                      <Navigation link="https://twitter.com/amitoser" isExternal>
                         Twitter
                       </Navigation>
                       <Navigation link="https://github.com/lukketsvane" isExternal>
