21e588ec9061ef2d9688666eef9dd5a1a7376250
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-04-03 01:13:34 +0000

    cleaning

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 // next.config.js
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  optimizeFonts: true,
-});
+const withMDX = require("@next/mdx")({ extension: /\.mdx?$/, options: { remarkPlugins: [], rehypePlugins: [], }, });
+module.exports = withMDX({ pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"], optimizeFonts: true, });
