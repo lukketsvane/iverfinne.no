03daaa9e0832e8fc30d28fdb478edd8ee804a167
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2024-05-22 22:36:11 +0000

    tagga

--- pages/polge.tsx
+++ pages/polge.tsx
@@ -28,17 +28,14 @@ const PolgePage: React.FC = () => {
           poster="/poster.webp"
           shadow-intensity="1"
         >
-          <button className="Hotspot" slot="hotspot-1" data-position="0.20224736591332104m 0.4323479749571223m 0.3647403278944923m" data-normal="0.8588368325207915m 0.33696497326740116m 0.3858158911923585m" data-visibility-attribute="visible">
-            <div className="HotspotAnnotation">Pøbelpølge</div>
+          <button className="Hotspot" slot="hotspot-1" data-position="-0.5557448879694129m 0.011800219002776835m 0.43415804527227114m" data-normal="-0.6430746019232203m -0.21018162844843763m 0.7363957763485982m" data-visibility-attribute="visible">
+            <div className="HotspotAnnotation">Eksemplar 1</div>
           </button>
-          <button className="Hotspot" slot="hotspot-2" data-position="-0.5641152805780563m 0.5962629611607801m 0.373158039107515m" data-normal="-0.8627435790114588m 0.04561406906184359m 0.5035800567716318m" data-visibility-attribute="visible">
-            <div className="HotspotAnnotation">Pyntepølge</div>
+          <button className="Hotspot" slot="hotspot-2" data-position="0.16548037345355704m 0.05017223443240476m 0.4544651560404812m" data-normal="0.8316606055151807m -0.34351864671153726m 0.43627465729239556m" data-visibility-attribute="visible">
+            <div className="HotspotAnnotation">Eksemplar 2</div>
           </button>
-          <button className="Hotspot" slot="hotspot-3" data-position="-0.0873609400787203m 0.5217595515167601m -0.3018671842072307m" data-normal="0.9631912269150061m 0.1905704188746901m 0.18959318511985124m" data-visibility-attribute="visible">
-            <div className="HotspotAnnotation">Rettutta dusjen pølge</div>
-          </button>
-          <button className="Hotspot" slot="hotspot-4" data-position="-0.06955537725458627m 0.3691717142367001m -0.09050702114499942m" data-normal="0.6645287206902761m 0.21700771255417733m 0.7150589011191656m" data-visibility-attribute="visible">
-            <div className="HotspotAnnotation">anatomisk korrekt pølge</div>
+          <button className="Hotspot" slot="hotspot-3" data-position="-0.10193927298295846m 0.015961774070006353m -0.3376911126766585m" data-normal="0.792607863399423m -0.08237612531739617m -0.6041414973787965m" data-visibility-attribute="visible">
+            <div className="HotspotAnnotation">Eksemplar 3</div>
           </button>
         </model-viewer>
       </div>
